7cd128530540f3d373d9aade4cb2f3f7f1db1e19
Amit Bilapatte <amit.bilapatte@squareyards.co.in>
2024-04-04 00:31:43 +0530

    remove unused code

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 "use client";
-import { ArrowRightIcon } from "@heroicons/react/24/outline";
-import Image from "next/image";
-import Link from "next/link";
+
 import { useState } from "react";
 
 const Details = [
